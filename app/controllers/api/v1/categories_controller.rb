92958c4672c28e8d503d9b65995796d688a706a7
yjspoder <injose.joshi@gmail.com>
2020-05-28 16:00:35 +0530

    Add create action to categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -5,8 +5,26 @@ class Api::V1::CategoriesController < ApplicationController
     render json: { category_list: fetch_category_list }
   end
 
+  def create
+    @category = Category.find_by(category_params)
+    if @category.exists?
+      render status: :ok, json: { notice: "The provided category already exists." }
+    else
+      category = Category.new(category_params)
+      if category.save
+        render status: :ok, json: { notice: "Category created successfully." }
+      else
+        render status: :unprocessable_entities, json: { errors: category.errors.full_messages }
+      end
+    end
+  end
+
   private
   def fetch_category_list
     @categories = Category.order(created_at: :desc)
+  end
+
+  def category_params
+    params.require(:category).permit(:title)
   end
 end
