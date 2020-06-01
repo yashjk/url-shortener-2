1609cb2fbaf2f8008571d89788f4aead42e4b95f
yjspoder <injose.joshi@gmail.com>
2020-06-01 18:36:34 +0530

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'homepage#index'
   namespace :api do
     namespace :v1 do
       resources :urls, only: [:index, :create, :update, :destroy]
       resources :categories, only: [:index, :create, :update, :destroy]
     end
   end
+  root 'homepage#index'
+  get '/*path' => 'homepage#index'
 end
