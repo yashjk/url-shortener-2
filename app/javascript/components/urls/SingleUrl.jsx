746f4c2fab4b18f7dae0b2ecdebb3de13d8ae565
yjspoder <injose.joshi@gmail.com>
2020-06-01 17:21:39 +0530

    Add dropdown to single url

--- app/javascript/components/urls/SingleUrl.jsx
+++ app/javascript/components/urls/SingleUrl.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
 import Pin from "./Pin";
+import Dropdown from "./Dropdown";
 
 class SingleUrl extends Component {
   constructor(props) {
     super(props);
   }
+
+  handleClick = (id) => {
+    const api = `/api/v1/urls/${id}`;
+    const url = {
+      pinned: !this.props.url.pinned,
+    };
+    fetch(api, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(url),
+    }).then((response) => this.props.handleResponse(response));
+  };
+
   render() {
-    const { original, shortened, pinned, category_id } = this.props.url;
+    const { original, shortened, pinned, category_id, id } = this.props.url;
     return (
       <tr>
         <th
-          className={pinned ? "" : "bi bi-alert-triangle text-success"}
+          className={pinned ? "bi bi-alert-triangle text-success" : ""}
           scope="row"
           style={{ cursor: "pointer" }}
           onClick={() => this.handleClick(id)}
@@ -19,7 +35,13 @@ class SingleUrl extends Component {
         </th>
         <td>{original}</td>
         <td>https://short.is/{shortened}</td>
-        <td>{category_id}</td>
+        <td>
+          <Dropdown
+            category_id={category_id}
+            url_id={id}
+            handleResponse={this.props.handleResponse}
+          />
+        </td>
       </tr>
     );
   }
