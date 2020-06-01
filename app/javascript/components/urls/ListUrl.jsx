346d39115107865e3bd087aab9532c09e6adef83
yjspoder <injose.joshi@gmail.com>
2020-06-01 08:37:43 +0530

    Add ListUrl component

---
+++ app/javascript/components/urls/ListUrl.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import SingleUrl from "./SingleUrl";
+
+class ListUrl extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Pin</th>
+            <th scope="col">Original</th>
+            <th scope="col">Shortened</th>
+            <th scope="col">Category</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.props.url_list &&
+            this.props.url_list.map((url) => {
+              return <SingleUrl key={url.id} url={url} />;
+            })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default ListUrl;
