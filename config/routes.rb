93f9d131b833accecc434405a9b2227cb666648e
yjspoder <injose.joshi@gmail.com>
2020-06-05 23:23:26 +0530

    Add count resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :urls, only: [:index, :create, :show, :update, :destroy]
       resources :categories, only: [:index, :create, :update, :destroy]
+      resources :counts, only: [:index]
     end
   end
   root 'homepage#index'
